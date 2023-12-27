49e258e643f2bb3815a166bee7b20231f115d017
MeiXiang <myhuong.nn@gmail.com>
2023-12-27 20:09:21 +0700

    Module 1

---
+++ script.js
@@ -0,0 +1,18 @@
+//Bai 1
+console.log("Hello World");
+
+//Bai 2
+const message = "Hello World";
+console.log(message);
+
+//Bai 3
+const hello = "Hello";
+const World = "World";
+const combineMessage = hello + " " + World;
+console.log(combineMessage);
+
+//Bai 4
+function printMessage(mes) {
+  console.log(mes);
+}
+printMessage(combineMessage);
